96a0c72c3226dd1f8053042fae944897c29911e5
danewton2010 <danewton@163.com>
2017-04-09 10:26:49 +0800

    setting mysql

--- untitled/settings.py
+++ untitled/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'untitled.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test',
+        'USER':'root',
+        'PASSWORD':'123456',
+        'HOST':'',
+        'PORT':'',
     }
 }
 
