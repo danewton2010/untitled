c7d0fd659155353eaf1cc91893fa174c61202a45
danewton2010 <danewton@163.com>
2017-04-10 17:49:51 +0800

    setting mysql

--- app/models.py
+++ app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class CCBZ(models.Model):
+    code = models.IntegerField()
+    name = models.CharField(max_length=30)
+    train = models.CharField(max_length=30)
+    start_time = models.DateTimeField()
+    stop_time = models.DateTimeField()
+    work_time = models.FloatField()
+    work_bonus = models.FloatField()
+    in_time = models.DateTimeField()
+    out_time = models.DateTimeField()
+    rest_time = models.FloatField()
+    rest_bonus = models.FloatField()
+    sum_bonus = models.FloatField()
--- untitled/urls.py
+++ untitled/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from app.views import index
+from app.views import index,input
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^index/', index),
+    url(r'^input/', input),
 ]
