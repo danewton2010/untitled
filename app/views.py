c938d56389962f0c0f737144dfb346093a9d9a1f
danewton2010 <danewton@163.com>
2017-04-17 17:12:40 +0800

    ajax

--- app/views.py
+++ app/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
+from django.shortcuts import render_to_response
+from django.shortcuts import HttpResponseRedirect
 
 # Create your views here.
 def index(request):
@@ -6,3 +9,8 @@ def index(request):
 
 def input(request):
     return render(request,"input.html")
+
+def add(request):
+    data = request.POST['html_data']
+    print(data)
+    return HttpResponse("add.html",{"html_data":data})
--- untitled/urls.py
+++ untitled/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from app.views import index,input
+from app.views import index,input,add
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^index/', index),
     url(r'^input/', input),
+    url(r'^add/', add),
 ]
