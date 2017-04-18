f283ece264c3213944b237bf921954a3757f3901
danewton2010 <danewton@163.com>
2017-04-18 15:25:18 +0800

    insert to database

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 from django.shortcuts import render_to_response
 from django.shortcuts import HttpResponseRedirect
+from django.db import connection,transaction
 import json
 
 # Create your views here.
@@ -12,10 +13,16 @@ def input(request):
     return render(request,"input.html")
 
 def add(request):
+    cursor = connection.cursor()
+    sql = 'insert into app_ccbz(code,name,train,start_time,stop_time,work_time,work_bonus,in_time,out_time,rest_time,rest_bonus,sum_bonus) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
+
     data = request.POST['html_data'].rstrip(u',删除').split(",")[12:]
     data=",".join(data)
     data = data.split(u'删除')
     for d in data:
-        print(d.strip(","))
+        d=d.strip(",")
+        sql_d = d.split(",")
+        cursor.execute(sql,sql_d)
+    transaction.commit()
 
     return HttpResponse(json.dumps({"html_data":data}))
