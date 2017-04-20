a8c2f7efa64a5ca2c281f9b6b20b17302e9ce32f
danewton2010 <danewton@163.com>
2017-04-20 10:32:09 +0800

    chart bar

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from app.models import CCBZ
+from django.apps import apps
+
 # Register your models here.
 
-admin.site.register(CCBZ)
+
+
+class CCBZ_admin(admin.ModelAdmin):
+    modelobj = apps.get_model("app","CCBZ")
+    filed = modelobj._meta.fields
+    list_display = [f.name for f in filed][1:]
+
+admin.site.register(CCBZ,CCBZ_admin)
--- app/models.py
+++ app/models.py
@@ -2,15 +2,17 @@ from django.db import models
 
 # Create your models here.
 class CCBZ(models.Model):
-    code = models.IntegerField()
-    name = models.CharField(max_length=30)
-    train = models.CharField(max_length=30)
-    start_time = models.DateTimeField()
-    stop_time = models.DateTimeField()
-    work_time = models.FloatField()
-    work_bonus = models.FloatField()
-    in_time = models.DateTimeField()
-    out_time = models.DateTimeField()
-    rest_time = models.FloatField()
-    rest_bonus = models.FloatField()
-    sum_bonus = models.FloatField()
+    code = models.IntegerField(verbose_name="编码")
+    name = models.CharField(max_length=30,verbose_name="姓名")
+    train = models.CharField(max_length=30,verbose_name="车次")
+    start_time = models.DateTimeField(verbose_name="出乘开始")
+    stop_time = models.DateTimeField(verbose_name="出乘结束")
+    work_time = models.FloatField(verbose_name="出乘时间")
+    work_bonus = models.FloatField(verbose_name="出乘补助")
+    in_time = models.DateTimeField(verbose_name="休息开始")
+    out_time = models.DateTimeField(verbose_name="休息结束")
+    rest_time = models.FloatField(verbose_name="休息时间")
+    rest_bonus = models.FloatField(verbose_name="休息补助")
+    sum_bonus = models.FloatField(verbose_name="合计补助")
+
+
